6180bda694d5c25325c549c185f394c74703f395
fvnky <brucewayne0002@proton.me>
2026-01-24 17:20:09 +0000

    feat: added custom tw colours

--- apps/website/app/(landing)/page.tsx
+++ apps/website/app/(landing)/page.tsx
@@ -1,13 +1,10 @@
+import Hero from '@/components/sections/landing/hero';
+
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-white dark:bg-black">
-      <main className="flex min-h-screen flex-col items-center justify-center px-4">
-        <h1 className="text-4xl font-bold text-black dark:text-white">
-          Welcome to Benchmrk
-        </h1>
-        <p className="mt-4 text-lg text-zinc-600 dark:text-zinc-400">
-          Your landing page content goes here
-        </p>
+    <div className="bg-black-2 flex min-h-screen items-center justify-center">
+      <main className="bg-black-1 max-w-vw m-6 flex min-h-screen w-full flex-col items-center justify-between border-2 border-red-600 bg-white p-6 sm:items-start dark:bg-black">
+        <Hero />
       </main>
     </div>
   );
---
+++ apps/website/components/sections/landing/hero.tsx
@@ -0,0 +1,3 @@
+export default function Hero() {
+  return <div className="bg-red-1 rounded-4xl p-4">hero section</div>;
+}
