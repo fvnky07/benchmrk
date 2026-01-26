5be7da932e84613ff04da7ffc96004d31f651c75
fvnky <brucewayne0002@proton.me>
2026-01-26 21:27:57 +0000

    feat: hero savepoint

--- apps/website/components/sections/landing/features.tsx
+++ apps/website/components/sections/landing/features.tsx
@@ -1,9 +1,9 @@
+'use client';
+
 export default function Features() {
   return (
     <section className="debug-5 flex h-screen w-full items-center justify-center px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
-        <h1 className="text-4xl font-bold text-white">Features</h1>
-      </div>
+      <div className="w-full max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl"></div>
     </section>
   );
 }
--- apps/website/components/sections/landing/waiting-list.tsx
+++ apps/website/components/sections/landing/waiting-list.tsx
@@ -1,6 +1,6 @@
 export default function WaitingList() {
   return (
-    <section className="flex h-screen w-full items-center justify-center px-4 sm:px-6 lg:px-8">
+    <section className="debug-3 flex h-screen w-full items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-sm sm:max-w-md md:max-w-2xl lg:max-w-5xl xl:max-w-7xl">
         <h1 className="text-4xl font-bold text-white">Waiting List</h1>
       </div>
